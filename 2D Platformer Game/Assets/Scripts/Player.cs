805fab68709cc0f03a54240151a0e6b10270bf29
Hanif Kukuh Raharjo <hraharjo27@gmail.com>
2024-03-16 06:38:36 +0800

    Add Project Base Build

---
+++ 2D Platformer Game/Assets/Scripts/Player.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [Header("Move Info")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    private bool canDoubleJump;
+
+    private float movingInput;
+
+    [Header("Collision Info")]
+    public LayerMask whatIsGround;
+    public float groundCheckDistance;
+    private bool isGrounded;
+
+
+    private bool facingRight = true;
+    private int facingDirection = 1;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        AnimationControllers();
+        FlipController();
+        CollisionChecks();
+        InputChecks();
+        
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        Move();
+    }
+
+    private void AnimationControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void InputChecks()
+    {
+        movingInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpButton();
+        }
+    }
+
+    private void JumpButton()
+    {
+        if (isGrounded)
+        {
+            Jump();
+        }
+        else if(canDoubleJump)
+        {
+            canDoubleJump = false;
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void FlipController()
+    {
+        if(facingRight && movingInput < 0)
+        {
+            Flip();
+        }
+        else if(!facingRight && movingInput > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection = facingDirection * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance, transform.position.z));    
+    }
+}
