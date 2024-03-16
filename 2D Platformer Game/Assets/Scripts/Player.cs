908c9012c0b4d0d2b98816ea3bce57633eaad097
Hanif Kukuh Raharjo <hraharjo27@gmail.com>
2024-03-16 17:07:58 +0800

    Add Wall Jump

--- 2D Platformer Game/Assets/Scripts/Player.cs
+++ 2D Platformer Game/Assets/Scripts/Player.cs
@@ -10,15 +10,21 @@ public class Player : MonoBehaviour
     [Header("Move Info")]
     public float moveSpeed;
     public float jumpForce;
+    public Vector2 wallJumpDirection;
 
-    private bool canDoubleJump;
+    private bool canDoubleJump = true;
+    private bool canMove;
 
     private float movingInput;
 
     [Header("Collision Info")]
     public LayerMask whatIsGround;
     public float groundCheckDistance;
+    public float wallCheckDistance;
     private bool isGrounded;
+    private bool isWallDetected;
+    private bool canWallSlide;
+    private bool isWallSliding;
 
 
     private bool facingRight = true;
@@ -41,8 +47,15 @@ public class Player : MonoBehaviour
         if (isGrounded)
         {
             canDoubleJump = true;
+            canMove = true;
         }
 
+        if(canWallSlide)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.1f);
+        }
+            
         Move();
     }
 
@@ -52,6 +65,8 @@ public class Player : MonoBehaviour
 
         anim.SetBool("isMoving", isMoving);
         anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isWallSliding", isWallSliding);
+        anim.SetBool("isWallDetected", isWallDetected);
         anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
@@ -59,15 +74,20 @@ public class Player : MonoBehaviour
     {
         movingInput = Input.GetAxisRaw("Horizontal");
 
+        if(Input.GetAxis("Vertical") < 0)
+            canWallSlide = false;
+        
         if (Input.GetKeyDown(KeyCode.Space))
-        {
             JumpButton();
-        }
     }
 
     private void JumpButton()
     {
-        if (isGrounded)
+        if(isWallSliding)
+        {
+            WallJump();
+        }
+        else if (isGrounded)
         {
             Jump();
         }
@@ -76,11 +96,20 @@ public class Player : MonoBehaviour
             canDoubleJump = false;
             Jump();
         }
+
+        canWallSlide = false;
     }
 
     private void Move()
     {
-        rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+        if(canMove)
+            rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+    }
+
+    private void WallJump()
+    {
+        canMove = false;
+        rb.velocity = new Vector2(wallJumpDirection.x * -facingDirection, wallJumpDirection.y);
     }
 
     private void Jump()
@@ -90,11 +119,11 @@ public class Player : MonoBehaviour
 
     private void FlipController()
     {
-        if(facingRight && movingInput < 0)
+        if(facingRight && rb.velocity.x < -.1f)
         {
             Flip();
         }
-        else if(!facingRight && movingInput > 0)
+        else if(!facingRight && rb.velocity.x > .1f)
         {
             Flip();
         }
@@ -110,10 +139,21 @@ public class Player : MonoBehaviour
     private void CollisionChecks()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDirection, wallCheckDistance, whatIsGround);
+
+        if(isWallDetected && rb.velocity.y < 0)
+            canWallSlide = true;
+
+        if(!isWallDetected)
+        {
+            isWallSliding = false;
+            canWallSlide = false;
+        }
     }
 
     private void OnDrawGizmos() 
     {
-        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance, transform.position.z));    
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance, transform.position.z));
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + wallCheckDistance * facingDirection, transform.position.y));    
     }
 }
