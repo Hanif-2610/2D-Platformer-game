3b6ab5abd9eb47e4343527976dfd098cc1391fbd
Hanif Kukuh Raharjo <hraharjo27@gmail.com>
2025-05-20 22:44:15 +0800

    Add Level Manager

---
+++ 2D Platformer Game/Assets/LevelManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject levelButton;
+    [SerializeField] private Transform levelButtonParent;
+
+    private void Start()
+    {
+        for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string sceneName = "Level " + i;
+            Debug.Log(sceneName);
+
+            GameObject newButton = Instantiate(levelButton, levelButtonParent);
+            newButton.AddComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = sceneName;  
+        }
+    }
+
+    public void LoadLevel(string sceneName) => SceneManager.LoadScene(sceneName);
+}
